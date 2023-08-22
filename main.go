c72911a1f51184193173bfdcca71feb0f9c6a010
HeadHunter <christian-gros@outlook.fr>
2023-08-22 16:17:11 +0200

    blablabla

--- main.go
+++ main.go
@@ -2,16 +2,14 @@
 package main
 
 import (
-	"https://github.com/Pepinonte/go-crud/blob/master/models"
+	"/Pepinonte/go-crud/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
     router := gin.Default()
 
-    models.ConnectDatabase()  // new!
-
-    // ...
+    models.ConnectDatabase() 
 
     router.Run("localhost:8080")
 }
